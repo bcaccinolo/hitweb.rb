e9b44df51e733433b43cc1ea09969ba769149143
Your full name <benoit.caccinolo@gmail.com>
2013-02-19 21:49:15 +0100

    update

--- index.rb
+++ index.rb
@@ -3,5 +3,12 @@ require 'sinatra'
 
 get '/' do
     "Hello World!"
+    stream do |out|
+      out << "It's gonna be legen -\n"
+      sleep 0.5
+      out << " (wait for it) \n"
+      sleep 1
+      out << "- dary!\n"
+    end
 end
 

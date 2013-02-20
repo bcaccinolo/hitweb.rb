865a1bda6477fb5daeec1af41bcce4c0d8aac3f7
bcaccinolo <benoit.caccinolo@gmail.com>
2013-02-20 16:22:23 +0100

    update

--- index.rb
+++ index.rb
@@ -37,13 +37,6 @@ end
 DataMapper.finalize.auto_upgrade!
 
 get '/' do
-    "Hello World!"
-    stream do |out|
-      out << "It's gonna be legen -\n"
-      sleep 0.5
-      out << " (wait for it) \n"
-      sleep 1
-      out << "- dary!\n"
-    end
+  "Hello World!"
 end
 

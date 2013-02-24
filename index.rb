caf967d3f2a54c812e554f606c6de27698ed37f8
Your full name <benoit.caccinolo@gmail.com>
2013-02-24 10:38:39 +0100

    create url

--- index.rb
+++ index.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 
 require 'sinatra'
 require 'data_mapper'
@@ -34,6 +35,65 @@ class Category
   belongs_to :parent, :model => 'Category'
 
   has n, :links
+
+  def self.clean
+
+  s = "Top/World/Français"
+  puts s
+  t = s.downcase
+  puts t
+
+  replacements = []
+  replacements << [/ /, '-']
+  replacements << [/\//, '-']
+
+  replacements << [/[çç]/, 'c']
+  replacements << [/ãåäàâá/, 'a']
+  replacements << [/[éè]/, 'e']
+
+  replacements << [/[a-z]/, '']
+
+  replacements.each do |r|
+    t = t.gsub(r[0], r[1])
+  end
+
+  puts t
+    
+  end
+  
+  def create_url 
+    
+    s = self.title
+    
+    
+    c = "Top/World/Français"
+    puts s
+    t = s.downcase
+
+    puts t
+
+    replacements = []
+    replacements << [/ãåäàâá/, 'a']
+    replacements << [/[éè]/, 'e']
+    replacements << [/Öôöó/, 'o']
+    replacements << [/ìÎíîï/, 'i']
+    replacements << [/üûùÜ/, 'u']
+    replacements << [/ÿ/, 'y']
+    replacements << [/Çç/, 'c']
+    replacements << [/ñ/, 'n']
+    replacements << [/ø/, 'o']
+    replacements << [/Œ/, 'o']
+    replacements << [/ş/, 's']
+    replacements << [/ /, '-']
+    replacements << [/\//, '-']
+
+    replacements.each do |r|
+      t = t.gsub(r[0], r[1])
+    end
+
+    puts t
+    t
+  end
 end
 
 DataMapper.finalize.auto_upgrade!
